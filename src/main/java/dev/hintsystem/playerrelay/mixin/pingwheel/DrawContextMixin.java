c365e06efa996fad0556e2c9071dbd5b62d12e53
HintSystem <kokins24jul@gmail.com>
2025-10-25 23:44:29 +0300

    Support ping wheel 1.12.0

--- src/main/java/dev/hintsystem/playerrelay/mixin/pingwheel/DrawContextMixin.java
+++ src/main/java/dev/hintsystem/playerrelay/mixin/pingwheel/DrawContextMixin.java
@@ -4,22 +4,20 @@ import dev.hintsystem.playerrelay.PlayerRelay;
 import dev.hintsystem.playerrelay.cast.pingwheel.DrawContextAccessor;
 import dev.hintsystem.playerrelay.payload.PlayerInfoPayload;
 
-import com.mojang.blaze3d.pipeline.RenderPipeline;
+import nx.pingwheel.common.render.DrawContext;
+
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.Text;
-import net.minecraft.util.Colors;
-import net.minecraft.util.Identifier;
-
-import nx.pingwheel.common.render.DrawContext;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.UUID;
 
-@Mixin(DrawContext.class)
+@Mixin(value = DrawContext.class, remap = false)
 public class DrawContextMixin implements DrawContextAccessor {
     @Unique
     private UUID playerrelay$authorId;
@@ -50,39 +48,29 @@ public class DrawContextMixin implements DrawContextAccessor {
         instance.drawText(textRenderer, text, x, y, newColor, newShadow);
     }
 
-    @Redirect(
-        method = "renderTexture",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIFFIIII)V"
-        )
+    @ModifyVariable(
+        method = "renderPing",
+        at = @At("HEAD"),
+        argsOnly = true,
+        ordinal = 0
     )
-    private void modifyTextureColor(net.minecraft.client.gui.DrawContext instance, RenderPipeline pipeline,
-                                    Identifier sprite, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight) {
-        int color = Colors.WHITE;
-
+    private int modifyPingColor(int color) {
         PlayerInfoPayload playerInfo = PlayerRelay.getConnectedPlayer(this.playerrelay$authorId);
-        if (playerInfo != null) color = playerInfo.getNameColor();
+        if (playerInfo != null) return playerInfo.getNameColor();
 
-        instance.drawTexture(pipeline, sprite, x, y, u, v, width, height, textureWidth, textureHeight, color);
+        return color;
     }
 
-    @Redirect(
-        method = "renderDefaultPingIcon",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V"
-        )
+    @ModifyVariable(
+        method = "renderArrowIcon",
+        at = @At("HEAD"),
+        argsOnly = true,
+        ordinal = 0
     )
-    private void modifyDefaultIconColor(
-        net.minecraft.client.gui.DrawContext instance,
-        int x1, int y1, int x2, int y2, int color
-    ) {
-        int newColor = color;
-
+    private int modifyArrowColor(int color) {
         PlayerInfoPayload playerInfo = PlayerRelay.getConnectedPlayer(this.playerrelay$authorId);
-        if (playerInfo != null) newColor = playerInfo.getNameColor();
+        if (playerInfo != null) return playerInfo.getNameColor();
 
-        instance.fill(x1, y1, x2, y2, newColor);
+        return color;
     }
 }
